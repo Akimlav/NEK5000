c64e0a64b600aff6672de382c616f3ff6fe0bc71
akimlav <akimlav@icloud.com>
2021-02-26 11:13:38 +0100

    stark01 fix

--- matrix.py
+++ matrix.py
@@ -14,7 +14,7 @@ import itertools
 
 
 # path = input() + '/'
-# path = ''
+path = '/home/afabret/data/room_deposition/production_run/roomBackUp00002/'
 # path = '../fbalance/'
 
 fileList = [name for name in listdir(path) if name.endswith(".3D")]
@@ -22,8 +22,8 @@ fileList.sort()
 
 # inpt =[['a', 0, 1, 5, path, fileList],['s', 0, -1, 5, path, fileList], ['s', 0, 1, 5, path, fileList], ['a', 0, 1, 5, path, fileList]]
 
-inpt = [['a', 0, 50, 5, path, fileList],['a', 0, 1000, 5, path, fileList],['a', 0, 2500, 5, path, fileList],['a', 0, -1, 5, path, fileList],['a', 0, -1, 5, path, fileList],
-        ['s', 0, 50, 5, path, fileList],['s', 0, 1000, 5, path, fileList],['s', 0, 2500, 5, path, fileList],['s', 0, -1, 5, path, fileList],['s', 0, -1, 5, path, fileList]]
+inpt = [['a', 0, 50, 5, path, fileList],['a', 0, 1000, 5, path, fileList],['a', 0, 2500, 5, path, fileList],['a', 0, 4000, 5, path, fileList],['a', 0, -1, 5, path, fileList],
+        ['s', 0, 50, 5, path, fileList],['s', 0, 1000, 5, path, fileList],['s', 0, 2500, 5, path, fileList],['s', 0, 4000, 5, path, fileList],['s', 0, -1, 5, path, fileList]]
 
 for i in range(len(inpt)):
     print(i)
@@ -44,21 +44,3 @@ for i in range(len(inpt)):
     fig.tight_layout()
     plt.colorbar(heatmap)
     plt.savefig(tit + '.png', dpi=200)
-    plt.show()
-
-
-# if choose == 'a':
-#     title = 'All particles' + ' t1 = ' + str(t1) +  ', t2 = ' + str(t2)
-#     tit = 'matrix_all' + '_'+ str(int(t1)) +  '_' + str(int(t2))
-#     heatmap = plt.pcolor(np.array(((0,2000),(0,2000))))
-# elif choose == 's':
-#     title = 'Sphere'  + ' t1 = ' + str(t1) +  ', t2 = ' + str(t2)
-#     tit = 'matrix_sphere'  + '_'+ str(int(t1)) +  '_' + str(int(t2))
-#     heatmap = plt.pcolor(np.array(((0,30),(0,30))))
-# fig, ax = plt.subplots(figsize=(7, 7))
-# im = ax.imshow(B)
-# ax.set_title(title)
-# fig.tight_layout()
-# plt.colorbar(heatmap)
-# # plt.savefig(tit + '.png', dpi=200)
-# plt.show()
